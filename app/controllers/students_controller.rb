6cca5efc834e64582d5ad28ec91eb388eadbea5a
River <+>
2022-06-29 23:29:42 -0400

    commit tree 913075dd44d2739e1da2b18fbde309366ea87a8d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if (!params[:name])
+      students = Student.all
+      render json: students
+    else
+      name = params[:name].camelcase
+      students = Student.where("last_name=?", name)
+      render json: students
+    end
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
